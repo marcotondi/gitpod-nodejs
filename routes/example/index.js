3f8f5a347267d179c3867b96904a3ba2d763127f
Marco Tondi <marcotondi@gmail.com>
2022-02-02 17:28:05 +0000

    first commit gitpod

--- routes/example/index.js
+++ routes/example/index.js
@@ -1,7 +1,9 @@
 'use strict'
 
 module.exports = async function (fastify, opts) {
+
+  fastify.log.info('Ciao da gitpod.io')
   fastify.get('/', async function (request, reply) {
-    return 'this is an example'
+    return 'Ciao da gitpod.io'
   })
 }
